df9b0d3293c168f7ded3fffcf75888bda670157b
MagdaDobre <magdadobre14@gmail.com>
2024-09-04 19:44:06 +0300

    Amterminat de refactorizat primul test.

--- src/test/java/tests/TabWindowTest.java
+++ src/test/java/tests/TabWindowTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import helpMethods.ElementMethods;
+import helpMethods.TabMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -21,21 +23,54 @@ public class TabWindowTest {
         driver.get("https://demoqa.com/");
         //facem browserul maximise
         driver.manage().window().maximize();
+
+        ElementMethods elementMethods =  new ElementMethods(driver);
+        TabMethods tabMethods = new TabMethods(driver);
+
         WebElement alertsFrameWindowMenu = driver.findElement(By.xpath("//h5[text()= 'Alerts, Frame & Windows']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();", alertsFrameWindowMenu);
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("arguments[0].click();", alertsFrameWindowMenu);
+        elementMethods.clickJSElement(alertsFrameWindowMenu);
 
         WebElement browserWindowSubMenu = driver.findElement(By.xpath("//span[text()='Browser Windows']"));
-        js.executeScript("arguments[0].click();", browserWindowSubMenu);
+//        js.executeScript("arguments[0].click();", browserWindowSubMenu);
+        elementMethods.clickJSElement(browserWindowSubMenu);
 
         WebElement newTabElement = driver.findElement(By.id("tabButton"));
-        js.executeScript("arguments[0].click();", newTabElement);
+//        js.executeScript("arguments[0].click();", newTabElement);
+        elementMethods.clickJSElement(newTabElement);
 
-        System.out.println("URL-ul paginii curente este:" + driver.getCurrentUrl());
-        List<String> tabsList = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(tabsList.get(1));
-        System.out.println("URL-ul paginii curente este:" + driver.getCurrentUrl());
-    }
+//        System.out.println("URL-ul paginii curente este:" + driver.getCurrentUrl());
+//
+//        List<String> tabsList = new ArrayList<>(driver.getWindowHandles());
+//        driver.switchTo().window(tabsList.get(1));
+//
+//        System.out.println("URL-ul paginii curente este:" + driver.getCurrentUrl());
+        tabMethods.switchSpecificTab(1);
 
+//        driver.close();
+        tabMethods.closeCurrentTab();
+
+//        driver.switchTo().window(tabsList.get(0));
+//        System.out.println("URL-ul paginii curente este " + driver.getCurrentUrl());
+        tabMethods.switchSpecificTab(0);
+
+        WebElement newWindowElement = driver.findElement(By.id("windowButton"));
+//        js.executeScript("arguments[0].click();", newWindowElement);
+        elementMethods.clickJSElement(newWindowElement);
+
+//        System.out.println("URL-ul paginii curente este " + driver.getCurrentUrl());
+//
+//        List<String> windowList = new ArrayList<>(driver.getWindowHandles());
+//        driver.switchTo().window(windowList.get(1));
+//
+//        System.out.println("URL-ul paginii curente este " + driver.getCurrentUrl());
+//
+//        driver.close();
+        tabMethods.switchSpecificTab(1);
+        tabMethods.closeCurrentTab();
+
+        driver.quit();
     }
+}
 
