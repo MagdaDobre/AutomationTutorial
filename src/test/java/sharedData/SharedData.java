9e614402f3af7b95f653843f09d4725aa5e7cf43
AlexDorha <alex_dorha@yahoo.com>
2024-10-09 20:42:58 +0300

    small refactoring for ci/cd

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -2,6 +2,7 @@ package sharedData;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,7 +13,13 @@ public class SharedData {
     private WebDriver driver;
 
     public void setUpDriver() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Run in headless mode for CI
+        options.addArguments("--no-sandbox"); // Required for GitHub Actions
+        options.addArguments("--disable-dev-shm-usage"); // Prevents memory issues
+        options.addArguments("--window-size=1920,1080"); // Set a default window size
+
+        driver = new ChromeDriver(options);
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
